f7ab14ce961fadfcce9c3bba1f1837e3da497c87
sukhansh mittal <112117834+mittalsukhansh@users.noreply.github.com>
2026-06-19 10:22:35 +0530

    Add files via upload

---
+++ codes/nrz/nrz.ino
@@ -0,0 +1,39 @@
+int ledPin = 13;
+int bitTime = 200;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+
+  Serial.println("Enter bit string:");
+}
+
+void sendNRZBit(char bit) {
+  if (bit == '1') {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(bitTime);
+}
+
+void loop() {
+  if (Serial.available()) {
+
+    String data = Serial.readStringUntil('\n');
+
+    Serial.print("Sending: ");
+    Serial.println(data);
+
+    for (int i = 0; i < data.length(); i++) {
+
+      if (data[i] == '0' || data[i] == '1') {
+        sendNRZBit(data[i]);
+      }
+
+    }
+
+    digitalWrite(ledPin, LOW);
+  }
+}
